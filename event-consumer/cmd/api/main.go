c3ac4604fa96c7d106b45eb3e749fa33b830f5f1
ignj <ignacio.jocano@gmail.com>
2022-09-05 21:43:42 -0300

    add serving support

--- event-consumer/cmd/api/main.go
+++ event-consumer/cmd/api/main.go
@@ -48,7 +48,7 @@ func main() {
 
 func connectToDB() *sql.DB {
 	// get from env
-	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=events sslmode=disable timezone=UTC connect_timeout=5"
+	dsn := "host=postgres-postgresql port=5432 user=postgres password=postgres dbname=events sslmode=disable timezone=UTC connect_timeout=5"
 
 	for {
 		connection, err := openDB(dsn)
